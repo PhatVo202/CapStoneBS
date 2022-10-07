dfade142e5a905b9b8777d101a75d22d518a53bf
PhatVo <9a10voletruongphat@gmail.com>
2022-10-08 03:31:32 +0700

    chinh sua contact about postpage responsive day du gan modal cho about

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
-document.getElementById("switchButton").onclick = function (){
-    document.getElementById("myBody").classList.toggle("dark");
-}
+document.getElementById("switchButton").onclick = function () {
+  document.getElementById("myBody").classList.toggle("dark");
+};
